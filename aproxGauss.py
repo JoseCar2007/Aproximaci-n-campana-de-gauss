4b05532631f56d3abd5be7dd82f75e706c41b3b0
JoseCar2007 <jctobar2007@gmail.com>
2025-11-18 16:35:50 -0500

    Cambio de nombre de variables

--- aproxGauss.py
+++ aproxGauss.py
@@ -1,14 +1,14 @@
 import math
 
 a = int(input("Ingrese el valor de a: "))
-deltaX = 20
-deltaY = 4
-nx = 2*a//deltaX
-ny = 2**a//deltaY
+nx = 20
+ny = 4
+deltaX = 2*a//nx
+deltaY = 2**a//ny
 aprox = 0
 def f(x: int, y: int) -> float:
     return math.pow(math.e, -x**2-y**2)
 for i in range(deltaY):
     for j in range(deltaX):
-        aprox += f(-a + j*nx, -a + i*ny)
+        aprox += f(-a + j*deltaX, -a + i*deltaY)
 print(aprox)
