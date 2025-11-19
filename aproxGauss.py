a8f3fd0982702c3b0ec4ef0b90013c1cea290a50
JoseCar2007 <jctobar2007@gmail.com>
2025-11-18 19:15:30 -0500

    Se agrego la multiplicacion por la base y altura

--- aproxGauss.py
+++ aproxGauss.py
@@ -8,7 +8,7 @@ deltaY = 2**a//ny
 aprox = 0
 def f(x: int, y: int) -> float:
     return math.pow(math.e, -x**2-y**2)
-for i in range(deltaY):
-    for j in range(deltaX):
-        aprox += f(-a + j*deltaX, -a + i*deltaY)
+for i in range(ny):
+    for j in range(nx):
+        aprox += f(-a + j*deltaX, -a + i*deltaY)*nx*ny
 print(aprox)
