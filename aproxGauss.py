f4cc949d286fbf8a35260074faec0595c0bc0c9d
JoseCar2007 <jctobar2007@gmail.com>
2025-11-20 09:12:22 -0500

    Se corrigió error en aproximación al aumentar el valor de nx y ny

--- aproxGauss.py
+++ aproxGauss.py
@@ -1,14 +1,16 @@
 import math
 
 a = int(input("Ingrese el valor de a: "))
-nx = 20
-ny = 4
-deltaX = 2*a//nx
-deltaY = 2**a//ny
+nx = 10000
+ny = 10000
+deltaX = 2*a/nx
+deltaY = 2*a/ny
 aprox = 0
-def f(x: int, y: int) -> float:
+
+def f(x, y):
     return math.pow(math.e, -x**2-y**2)
+
 for i in range(ny):
     for j in range(nx):
-        aprox += f(-a + j*deltaX, -a + i*deltaY)*nx*ny
+        aprox+=f(-a + j*deltaX, -a + i*deltaY)*deltaX*deltaY
 print(aprox)
